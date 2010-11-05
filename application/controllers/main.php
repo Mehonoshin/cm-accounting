a20d382ce0f86ffa12ab24bc9c12607d949ba781
ejabberd <ejabberd@d9db86c3-bff9-b27b-8038-378fb671a689>
2010-11-05 09:26:49 +0000

    Added CI, JQuery and sample main controller

---
+++ application/controllers/main.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Main extends Controller
+    {
+
+        function Main()
+        {
+            parent::Controller();
+        }
+		
+        function index()
+        {        	
+          $this->load->view('main_tpl');            
+        }
+    }
+
+?>
