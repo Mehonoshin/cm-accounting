214ec0559d4643b8a5e1ea8221ee473f225c429c
ejabberd <ejabberd@d9db86c3-bff9-b27b-8038-378fb671a689>
2011-01-07 19:44:56 +0000

    RC1

--- application/config/database.php
+++ application/config/database.php
@@ -1,10 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$env_used = 'default'; //name of your development setting
-if(defined('CIUnit_Version')){
-  $env_used .= '_test';
-}
-$active_group = $env_used;
+$active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
@@ -19,17 +15,3 @@ $db['default']['cache_on'] = "";
 $db['default']['cachedir'] = "";
 $db['default']['char_set'] = "utf8";
 $db['default']['dbcollat'] = "utf8_general_ci";
-
-
-$db['default_test']['hostname'] = "localhost";
-$db['default_test']['username'] = "root";
-$db['default_test']['password'] = "root";
-$db['default_test']['database'] = "acc_test";
-$db['default_test']['dbdriver'] = "mysql";
-$db['default_test']['dbprefix'] = "";
-$db['default_test']['pconnect'] = "1";
-$db['default_test']['db_debug'] = "1";
-$db['default_test']['cache_on'] = "";
-$db['default_test']['cachedir'] = "";
-$db['default_test']['char_set'] = "utf8";
-$db['default_test']['dbcollat'] = "utf8_general_ci";
